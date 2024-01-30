cef32f956183bf1f7e59f0f67f97ed6b3727e6f9
Dominik Roszkowski <dominik@roszkowski.dev>
2024-01-30 18:07:13 +0100

    chore: disable state preservation

--- sources/iOS/ios-communications/Sources/iOSCommunications/ble/endpoints/corebluetooth/central/CBDeviceListenerImpl.swift
+++ sources/iOS/ios-communications/Sources/iOSCommunications/ble/endpoints/corebluetooth/central/CBDeviceListenerImpl.swift
@@ -6,9 +6,10 @@ import RxSwift
 public class CBDeviceListenerImpl: NSObject, CBCentralManagerDelegate {
     private let SESSION_TEAR_DOWN_TIMEOUT_MS = 1000
   
-    fileprivate lazy var manager = CBCentralManager(delegate: self, queue: queueBle, options: [
-      CBCentralManagerOptionRestoreIdentifierKey: "PolarBleSDKCBCentralManagerOptionRestoreIdentifierKey"
-    ])
+    // fileprivate lazy var manager = CBCentralManager(delegate: self, queue: queueBle, options: [
+    //   CBCentralManagerOptionRestoreIdentifierKey: "PolarBleSDKCBCentralManagerOptionRestoreIdentifierKey"
+    // ])
+    fileprivate lazy var manager = CBCentralManager(delegate: self, queue: queueBle)
   
     public func centralManager(_ central: CBCentralManager, willRestoreState dict: [String : Any]) {
     BleLogger.trace("willRestoreState called with dict: \(dict)")
